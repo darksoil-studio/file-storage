b33d4760bdc5b8944000c389666c1de6fa8dd6ca
guillem.cordoba <guillem.cordoba@gmail.com>
2024-11-07 17:32:49 +0100

    nixConfig

--- flake.nix
+++ flake.nix
@@ -9,6 +9,17 @@
     p2p-shipyard.url = "github:darksoil-studio/p2p-shipyard/main-0.3";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://holochain-ci.cachix.org"
+      "https://darksoil-studio.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8="
+      "darksoil-studio.cachix.org-1:UEi+aujy44s41XL/pscLw37KEVpTEIn8N/kn7jO8rkc="
+    ];
+  };
+
   outputs = inputs@{ ... }:
     inputs.holonix.inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       # Import all `dnas/*/dna.nix` files
